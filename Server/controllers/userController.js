f9a5f6a0bc463a48cf1b3e050f2e988e30f7a555
odddyyy <56987696+odddyyy@users.noreply.github.com>
2020-03-06 00:46:31 +0700

    register, login, google login backend

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,6 +1,9 @@
 const { User } = require(`../models`)
 const sendGrid = require(`../helpers/sendGrid`)
 const createToken = require(`../helpers/token`)
+const {OAuth2Client} = require(`google-auth-library`)
+const client = new OAuth2Client(process.env.G_CLIENT_ID)
+
 
 class UserController {
 
@@ -54,7 +57,53 @@ class UserController {
     static googleSign (req, res, next) {
         let googleToken = req.body.token
         let payload = null
-        
+        client.verifyIdToken({
+            idToken: googleToken,
+            audience: process.env.G_CLIENT_ID
+        })
+        .then(data => {
+            payload = data.getPayload()
+            return User.findOne({where:{email:payload.email}})
+        })
+        .then(data => {
+            let createUser = {
+                id:data.id,
+                username:data.username,
+                email:data.email,
+                password:`rahasiaPribadiSaya`
+            }
+
+            if (data == null) {
+                User.create(createUser)
+                .then(data => {
+                    let user = {
+                        id:data.id,
+                        username:data.username,
+                        email:data.email
+                    }
+
+                    let tokenServer = createToken(user)
+                    res.status(200).json(token)
+                })
+            } else {
+                let gotToken = createToken({
+                    id:data.id,
+                    username:data.username,
+                    email:data.email
+                })
+
+                req.headers = gotToken
+                req.userData = {
+                    id:createUser.id,
+                    username:createUser.username,
+                    email:createUser.email
+                }
+                res.status(200).json(gotToken)
+            }
+        })
+        .catch(err => {
+            next(err)
+        })
     }
 
 }
