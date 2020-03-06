92990260cb764111479dd5f3d444158104c87b23
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-06 15:58:26 +0700

    finalisasi halaman bet room

--- Server/controllers/betController.js
+++ Server/controllers/betController.js
@@ -13,7 +13,22 @@ class betController{
         }
         let decoded=jwt.decode(token,process.env.SECRET_TOKEN || '12345');
         data.userId=decoded.id;
+
+        
         Bet.create(data).then(function(d){
+
+            var Pusher = require('pusher');
+
+            var pusher = new Pusher({
+                appId: '958794',
+                key: 'cf1f6fed1fb547b2cfdb',
+                secret: '5a72bbe35853892b3cd1',
+                cluster: 'ap1',
+                encrypted: true
+            });
+
+            pusher.trigger('my-channel', 'my-event',data);
+
             res.status(201).json(d);
         }).catch(err=>{
             next(err);
