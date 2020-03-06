2478fc0375489ed07e1d57d3850a71167b5d3394
hitsamtiammar123 <hitsamtiammmar@gmail.com>
2020-03-06 14:56:02 +0700

    penambahan default secret jika secret tidak ada

--- Server/helpers/token.js
+++ Server/helpers/token.js
@@ -1,7 +1,7 @@
 const jwt = require(`jsonwebtoken`)
 
 function createToken (userData) {
-    return jwt.sign(userData, process.env.SECRET_TOKEN)
+    return jwt.sign(userData, process.env.SECRET_TOKEN || '12345' )
 }
 
 module.exports = createToken
